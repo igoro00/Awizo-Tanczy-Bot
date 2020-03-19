ffb78c1913e9a0d904e6f3d13a0d6c5faec35280
igoro00 <igor.ordecha@gmail.com>
2020-03-19 17:48:40 +0100

    auto-create files folder if it doesnt exit prevents crashing if files folder not created

--- bot.py
+++ bot.py
@@ -4,14 +4,18 @@ from telegram.ext import MessageHandler, Filters, CommandHandler, Updater
 from telegram import ChatAction
 from datetime import datetime
 from key import key
-
+from os import makedirs
+try:
+    makedirs("files")
+except:
+    pass
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-#Create key.py file and type "key = {YOUR KEY}"
-updater = Updater(token=key, use_context=True) 
+#Create key.py file and type 'key = "{YOUR KEY}"' and replace {YOUR KEY} with your actual key
+updater = Updater(token=key, use_context=True)
 dispatcher = updater.dispatcher
 
 
@@ -19,7 +23,7 @@ logging.basicConfig(filename='logged_messages.log', format='%(asctime)s - %(mess
                      level=logging.INFO)
 
 def start(update, context):
-    update.message.reply_text("Wyślij plik dźwiękowy* a odeślę ci film jak awizo tańczy do tej piosenki.\n\n* Narazie obsługiwane tylko mp3. Jeśli audio jest dluższe od filmu(ok. 8s) to wykorzystane będzie tylko początek.")
+    update.message.reply_text("Wyślij plik dźwiękowy a odeślę ci film jak awizo tańczy do tej piosenki.\n\nJeśli audio jest dluższe od filmu(ok. 8s) to wykorzystane będzie tylko początek.\nBTW póki mam miejsce na dysku to sobie zachowuje te przeróbki.")
 
 def messageResponder(update, context):
     context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.UPLOAD_VIDEO)
